69c8bbc7bfc841408e5ee7ef582c72ff9789b897
Caio Feitosa <franciscocaiof@gmail.com>
2023-12-31 17:37:03 -0300

    feat: funcao para cadastro de voluntario no arquivo 'models'

--- models.py
+++ models.py
@@ -18,8 +18,24 @@ def get_colunas_tabela(nome_tabela: str) -> list:
     return [resultado[1] for resultado in resultados]
 
 
-def get_placeholders(colunas: list) -> str:
+def get_placeholders(colunas: list, usa_colunas: bool) -> str:
+    if usa_colunas:
+        return ', '.join(colunas)
     return ', '.join(['?' for _ in range(len(colunas))])
+
+
+def preparar_query(nome_tabela: str, lista_filtros: list) -> str:
+    clausura_where = ''
+    for filtro in lista_filtros:
+        if filtro:
+            if clausura_where:
+                clausura_where += " AND "
+            clausura_where += filtro
+
+    query = f"SELECT * FROM {nome_tabela}"
+    query += f" WHERE {clausura_where}" if clausura_where else ""
+
+    return query
 
 
 def abrir_conexao():
@@ -32,6 +48,22 @@ def fechar_conexao(conexao, commit: bool = True):
     if commit:
         conexao.commit()
     conexao.close()
+
+
+def set_voluntaio(dados: dict):
+    nome_tabela = 'voluntario'
+    colunas = get_colunas_tabela(nome_tabela)
+    conexao, cursor = abrir_conexao()
+    valores = tuple(dados.values())
+    query = f"""INSERT INTO {nome_tabela} (
+            {get_placeholders(colunas[1:], True)}
+        ) VALUES (
+            {get_placeholders(colunas[1:], False)}
+        )
+    """
+
+    cursor.execute(query, valores)
+    fechar_conexao(conexao)
 
 
 def get_voluntario(usuario: str) -> dict:
@@ -55,18 +87,7 @@ def listar_voluntarios(codigo: int = 0, nome: str = '') -> list:
     lista_filtros.append(f'codigo = {codigo}' if codigo else '')
     lista_filtros.append(f'nome = {nome}' if nome else '')
 
-    clausura_where = ''
-    for filtro in lista_filtros:
-        if filtro:
-            if clausura_where:
-                clausura_where += " AND "
-            clausura_where += filtro
-
-    if clausura_where:
-        query = f"SELECT * FROM {nome_tabela} WHERE {clausura_where}"
-    else:
-        query = f"SELECT * FROM {nome_tabela}"
-
+    query = preparar_query(nome_tabela, lista_filtros)
     cursor.execute(query)
     resultado = cursor.fetchall()
     fechar_conexao(conexao, False)
@@ -87,17 +108,10 @@ def set_crianca(dados: dict):
     conexao, cursor = abrir_conexao()
     valores = tuple(dados.values())
     query = f"""INSERT INTO {nome_tabela} (
-            {colunas[1]},
-            {colunas[2]},
-            {colunas[3]},
-            {colunas[4]},
-            {colunas[5]},
-            {colunas[6]},
-            {colunas[7]},
-            {colunas[8]},
-            {colunas[9]},
-            {colunas[10]}
-        ) VALUES ({get_placeholders(colunas[1:])})
+            {get_placeholders(colunas[1:], True)}
+        ) VALUES (
+            {get_placeholders(colunas[1:], False)}
+        )
     """
 
     cursor.execute(query, valores)
@@ -135,18 +149,7 @@ def listar_criancas(cpf: int = 0, nome: str = '', escola: str = '', idades: dict
         else:
             lista_filtros.append(f'idade BETWEEN {idade_inicial} AND {idade_final}')
 
-    clausura_where = ''
-    for filtro in lista_filtros:
-        if filtro:
-            if clausura_where:
-                clausura_where += " AND "
-            clausura_where += filtro
-
-    if clausura_where:
-        query = f"SELECT * FROM {nome_tabela} WHERE {clausura_where}"
-    else:
-        query = f"SELECT * FROM {nome_tabela}"
-
+    query = preparar_query(nome_tabela, lista_filtros)
     cursor.execute(query)
     resultado = cursor.fetchall()
     fechar_conexao(conexao, False)
@@ -163,26 +166,40 @@ def listar_criancas(cpf: int = 0, nome: str = '', escola: str = '', idades: dict
 
 
 # print(listar_voluntarios())
-# print(listar_criancas(idades={'idade1': 23, 'idade2': 30}))
-print(listar_criancas(nome='ana'))
+# print(listar_criancas(idades={'idade1': 10, 'idade2': 30}))
+# print(listar_criancas(nome='ana'))
+
+
+# colunas = get_colunas_tabela('voluntario')
+# print(get_placeholders(colunas, True))
+
 
 # print(get_voluntario('caio'))
 # print(get_crianca(1))
 # print(get_colunas_tabela('crianca'))
 
 
-# colunas = get_colunas_tabela('crianca')
-# set_crianca({
-#     colunas[1]: 'ana',
-#     colunas[2]: 444,
-#     colunas[3]: 555,
-#     colunas[4]: 'rua 2, casa 10',
-#     colunas[5]: 'francisco prado',
-#     colunas[6]: '5 ano do ensino fundamental',
-#     colunas[7]: '12345678',
-#     colunas[8]: '',
-#     colunas[9]: 12,
-#     colunas[10]: '2002-23-03'
+colunas = get_colunas_tabela('crianca')
+set_crianca({
+    colunas[1]: 'gabriel',
+    colunas[2]: 777,
+    colunas[3]: 999,
+    colunas[4]: 'rua 3, casa 8',
+    colunas[5]: 'francisco prado',
+    colunas[6]: '6 ano do ensino fundamental',
+    colunas[7]: '12345678',
+    colunas[8]: '99999999',
+    colunas[9]: 10,
+    colunas[10]: '2013-15-09',
+    colunas[11]: 'foto exemplo',
+})
+
+
+# colunas = get_colunas_tabela('voluntario')
+# set_voluntaio({
+#     colunas[1]: 'Jardel',
+#     colunas[2]: 'Diretoria',
+#     colunas[3]: 'jardel',
+#     colunas[4]: 'n0v4cr14tur4',
+#     colunas[5]: 'diretoria'
 # })
-
-
